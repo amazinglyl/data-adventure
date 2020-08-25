6a7f86225b24d8d5d04aad58730377c57724078a
Yilong Luan <yilong.luan@gmail.com>
2020-08-25 20:06:54 +0000

    Add natural language analyzer servlet.

---
+++ app/src/main/java/com/servlets/NaturalLanguageAnalyzer.java
@@ -0,0 +1,40 @@
+package com.servlets;
+
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.Sentiment;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** 
+* Input: landmark info in file "/WEB-INF/landmark-info.csv".
+* Output: sentiment score, which is between -1 and 1.
+* The sentiment score means how negative or positive the text is.
+*/
+@WebServlet("/sentiment")
+public class NaturalLanguageAnalyzer extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String message = request.getParameter("message");
+
+    // Create a new Document that contains the message entered by the user.
+    Document doc =
+        Document.newBuilder().setContent(message).setType(Document.Type.PLAIN_TEXT).build();
+    LanguageServiceClient languageService = LanguageServiceClient.create();
+    Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
+    float score = sentiment.getScore();
+    languageService.close();
+
+    // Output the sentiment score as HTML.
+    // A real project would probably store the score alongside the content.
+    response.setContentType("text/html;");
+    response.getWriter().println("<h1>Sentiment Analysis</h1>");
+    response.getWriter().println("<p>You entered: " + message + "</p>");
+    response.getWriter().println("<p>Sentiment analysis score: " + score + "</p>");
+    response.getWriter().println("<p><a href=\"/\">Back</a></p>");
+  }
+}
