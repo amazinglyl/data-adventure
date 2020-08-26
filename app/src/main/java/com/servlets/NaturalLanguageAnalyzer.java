1be89296f9dd0d887bf556360b7ab2736bbee499
Yilong Luan <yilong.luan@gmail.com>
2020-08-26 00:25:20 +0000

    Add NaturalLanguageAnalyzer servlet.

--- app/src/main/java/com/servlets/NaturalLanguageAnalyzer.java
+++ app/src/main/java/com/servlets/NaturalLanguageAnalyzer.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /** 
-* Input: landmark info in file "/WEB-INF/landmark-info.csv".
+* Input: user input from the webpage.
 * Output: sentiment score, which is between -1 and 1.
 * The sentiment score means how negative or positive the text is.
 */
@@ -24,10 +24,24 @@ public class NaturalLanguageAnalyzer extends HttpServlet {
     // Create a new Document that contains the message entered by the user.
     Document doc =
         Document.newBuilder().setContent(message).setType(Document.Type.PLAIN_TEXT).build();
-    LanguageServiceClient languageService = LanguageServiceClient.create();
+    
+    languageService = LanguageServiceClient.create();
     Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
     float score = sentiment.getScore();
     languageService.close();
+
+    // #1
+    // String sessionId = (String) request.getSession().getId();
+    // Credentials myCredentials = CredentialRetrival.GetCredentials(sessionId);
+    // LanguageServiceSettings languageServiceSettings =
+    //  LanguageServiceSettings.newBuilder()
+    //      .setCredentialsProvider(FixedCredentialsProvider.create(myCredentials))
+    //      .build();
+    // LanguageServiceClient languageServiceClient =
+    // LanguageServiceClient.create(languageServiceSettings);
+
+    // #2
+    
 
     // Output the sentiment score as HTML.
     // A real project would probably store the score alongside the content.
@@ -37,4 +51,12 @@ public class NaturalLanguageAnalyzer extends HttpServlet {
     response.getWriter().println("<p>Sentiment analysis score: " + score + "</p>");
     response.getWriter().println("<p><a href=\"/\">Back</a></p>");
   }
+
+//   private GoogleCredentials LoadCredentials() {
+//       // You can specify a credential file by providing a path to GoogleCredentials.
+//   // Otherwise credentials are read from the GOOGLE_APPLICATION_CREDENTIALS environment variable.
+//   GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(jsonPath))
+//         .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
+//     return credentials;
+//   }
 }
