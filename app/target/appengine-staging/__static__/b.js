20a5b3a888aa90cc5b55e560d526ecb3d2d17449
Yilong Luan <yilong.luan@gmail.com>
2020-08-30 01:00:51 +0000

    Fix link.

--- app/target/appengine-staging/__static__/b.js
+++ app/target/appengine-staging/__static__/b.js
@@ -1,8 +1,10 @@
 document.write(`
 
+<h1>Welcome!</h1>
+
 <nav class="navbar navbar-fixed-top navbar-expand-sm navbar-light bg-light mb-3">
     <div class="container">
-        <a class="navbar-brand" href="index.html">Data Adventure</a>
+        <a class="navbar-brand" href="#">Data Adventure</a>
         <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav"><span
                 class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarNav">
@@ -11,13 +13,16 @@ document.write(`
                     <a class="nav-link" href="index.html">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="dashboard.html">Dashboards</a>
+                    <a class="nav-link" href="dashboard.html">Dashboard</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="about.html">About</a>
+                    <a class="nav-link" href="faq">FAQs</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="contact.html">Contact</a>
+                    <a class="nav-link" href="contact">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="careers">Careers</a>
                 </li>
             </ul>
         </div>
--- app/target/appengine-staging/__static__/script.js
+++ app/target/appengine-staging/__static__/script.js
@@ -1,72 +1,9 @@
 /**
- * Map related functions.
- */
-
-/** Call Google Maps API, create a map and add landmarks. */
-function createMap() {
-    // Add a map.
-    const map = new google.maps.Map(
-        document.getElementById('map'),
-        {
-            center: {lat: -33.8688, lng: 151.2093},
-            zoom: 12
-        });
-
-    // Add a marker.
-    const trexMarker = new google.maps.Marker({
-        position: {lat: -33.8688, lng: 151.2093},
-        map: map,
-        title: 'Welcome vistors!'
-    });
-
-    // Add an info window.
-    const trexInfoWindow =
-        new google.maps.InfoWindow({content: 'This is Sydney.'});
-    trexInfoWindow.open(map, trexMarker);
-
-    // For each landmark, show an info window when clicked.
-    fetch('/landmarks').then(response => response.json()).then((landmarks) => {
-        landmarks.forEach((landmark) => {
-            addLandmarkToMap(map, landmark)
-        });
-    });  
-
-}
-
-/** For landscapes, add a marker that shows an info window when clicked. */
-function addLandmarkToMap(map, landmark) {
-  // Create a marker.
-  const marker = new google.maps.Marker({
-    position: {lat: landmark.lat, lng: landmark.lng},
-    map: map, 
-    title: landmark.title
-  });
-
-  // Create an info window for the marker.
-  const contentString = landmark.title + 
-        ': ' + landmark.description;
-
-  const infoWindow = new google.maps.InfoWindow({content: contentString});
-  marker.addListener('click', () => {
-    infoWindow.open(map, marker);
-  });
-}
-
-/**
  * Charts related functions.
  */
 google.charts.load('current', {'packages':['corechart']});
 // google.charts.setOnLoadCallback(drawVistorsChart);
 google.charts.setOnLoadCallback(drawCovid19Chart);
-
-// /** Fetches landmark-info and uses it to create a chart. */
-// function drawChart() {
-//   // Num. visitors chart.
-//   drawVistorsChart();
-
-//   // Covid19 chart
-//   drawCovid19Chart();
-// }
 
 /** Fetches landmark-info and uses it to create a chart. */
 function drawVistorsChart() {
